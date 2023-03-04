27e11c2e3b907ee145400d2843df302a802e357d
VitaliHD <122957465+VitaliHD@users.noreply.github.com>
2023-03-04 17:37:14 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5707058127:AAE-RgJCi6l5EVVoPGlEt8Xa2c-qBfeLTB0"
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'юань': 'CNY',
+    'фунт': 'GBP'
+}
---
+++ extensions.py
@@ -0,0 +1,39 @@
+
+import requests
+from config import exchanges
+
+
+class APIException(Exception):
+    pass
+
+
+class Convertor:
+    @staticmethod
+    def get_price(base, sym, amount):
+        try:
+            base_key = exchanges[base.lower()]
+        except KeyError:
+            raise APIException(f"Валюта {base} не найдена!")
+
+        try:
+            sym_key = exchanges[sym.lower()]
+        except KeyError:
+            raise APIException(f"Валюта {sym} не найдена!")
+
+        if base_key == sym_key:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}!')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}!')
+
+        url = (f'https://api.apilayer.com/exchangerates_data/latest?base={base_key}&symbols={sym_key}')
+        headers = {
+            "apikey": "KwKNpJzVt7UUzSxOEwcOl1wuBYJiIFev"
+        }
+        resp = requests.get(url, headers=headers).json()
+        new_price = resp['rates'][sym_key] * amount
+        new_price = round(new_price, 3)
+        message = f"Цена {amount} {base} в {sym} : {new_price}"
+        return message
